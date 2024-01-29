c815e58e3fdc1aaff0a4ac0d3dfda9af1f1e3161
Dhananjay <singhdhananjay.2001@gmail.com>
2024-01-29 21:26:04 +0530

    Change Success page to success

--- pages/api/payment.js
+++ pages/api/payment.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     payment_method_types: ["card"],
     line_items: transformedItems,
     mode: "payment",
-    success_url: `${process.env.HOST}/Success`,
+    success_url: `${process.env.HOST}/success`,
     cancel_url: `${process.env.HOST}/cart`,
     metadata: {
       userId,
