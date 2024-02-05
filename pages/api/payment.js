3c3bf8a34d1f347f08ea5d55025fb365b689fd00
Dhananjay <singhdhananjay.2001@gmail.com>
2024-02-05 21:00:51 +0530

    changing usd to inr back

--- pages/api/payment.js
+++ pages/api/payment.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const transformedItems = cartDetails.map((item) => ({
     quantity: 1,
     price_data: {
-      currency: "usd",
+      currency: "inr",
       unit_amount: item.price * 100,
       product_data: {
         name: item.title,
