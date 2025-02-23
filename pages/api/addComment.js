9da07c22e54e388896f7708527f7c643b60fca3a
Dhananjay Nathawat <singhdhananjay.2001@gmail.com>
2025-02-23 20:18:11 +0530

    just refracted addCommentapi code

--- pages/api/addComment.js
+++ pages/api/addComment.js
@@ -19,25 +19,27 @@ export default async function handler(req, res) {
     message,
   };
 
-  if (req.method === "POST") {
-    const { _id: commentId } = await client.create(newCommentDoc);
-    const r = await client
-      .patch(courseId)
-      // Ensure that the `reviews` arrays exists before attempting to add items to it
-      .setIfMissing({ comment: [] })
-      // Add the items after the last item in the array (append)
-      .insert("after", "comment[-1]", [{ _type: "reference", _ref: commentId }])
-      .commit({
-        // Adds a `_key` attribute to array items, unique within the array, to
-        // ensure it can be addressed uniquely in a real-time collaboration context
-        autoGenerateArrayKeys: true,
-      });
-    const q1 = getCommentQuery(commentId);
-    const newComment = await client.fetch(q1);
-    res.status(200).json(newComment);
-    try {
-    } catch (error) {
-      res.status(500).json(error.message);
+  try {
+    if (req.method === "POST") {
+      const { _id: commentId } = await client.create(newCommentDoc);
+      const r = await client
+        .patch(courseId)
+        // Ensure that the `reviews` arrays exists before attempting to add items to it
+        .setIfMissing({ comment: [] })
+        // Add the items after the last item in the array (append)
+        .insert("after", "comment[-1]", [
+          { _type: "reference", _ref: commentId },
+        ])
+        .commit({
+          // Adds a `_key` attribute to array items, unique within the array, to
+          // ensure it can be addressed uniquely in a real-time collaboration context
+          autoGenerateArrayKeys: true,
+        });
+      const q1 = getCommentQuery(commentId);
+      const newComment = await client.fetch(q1);
+      res.status(200).json(newComment);
     }
+  } catch (error) {
+    res.status(500).json(error.message);
   }
 }
